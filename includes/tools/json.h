1e434d97a2e552bd1e38770a06c404d20eaa2d2b
QinBuRua <114205033+QinBuRua@users.noreply.github.com>
2025-10-01 10:09:22 +0800

    Update

---
+++ includes/tools/json.h
@@ -0,0 +1,10 @@
+//
+// Created by QinBu_Rua on 2025/10/1.
+//
+
+#ifndef AUTO_SORTING_MACHINE_JSON_H
+#define AUTO_SORTING_MACHINE_JSON_H
+
+#include "../../submodules/Json/code/json.h"
+
+#endif //AUTO_SORTING_MACHINE_JSON_H
